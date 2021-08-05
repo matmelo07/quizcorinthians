2282f07f1d24be168e8782c16d021295da250a0b
matmelo07 <mateusmelo695@gmail.com>
2021-08-05 18:15:35 -0300

    initial commit

---
+++ questions.js
@@ -0,0 +1,96 @@
+let questions = [
+    {
+        question: 'Contra quem o corinthians foi campeao mundial em 2000?',
+        options: [
+            'Real madrid',
+            'Vasco da gama',
+            'Palmeiras',
+            'Chelsea'
+        ],
+        answer: 1
+    },
+    {
+        question: 'Em que ano foi inaugurado a arena do corinthians?',
+        options: [
+            '2010',
+            '2016',
+            '2014'
+        ],
+        answer: 2
+    },
+    {
+        question: 'Qual o jogador com mais jogos pelo corinthians?',
+        options: [
+            'Cassio"',
+            'Marcelinho carioca"',
+            'Wladimir"'
+        ],
+        answer: 2
+    },
+    {
+        question: 'Quantos mundiais tem o corinthians',
+        options: [
+            '2',
+            '1',
+            'nenhum'
+        ],
+        answer: 0
+    },
+    {
+        question: 'Contra quem o corinthians ganhou a libertadores da ameria?',
+        options: [
+            'Flamengo',
+            'Sao paulo',
+            'Boca juniors',
+            'River plate'
+        ],
+        answer: 2
+    },
+    {
+        question: 'Qual o ano da libertadores do corinthians?',
+        options: [
+            '2012',
+            '2000',
+            '2017'
+        ],
+        answer: 0
+    },
+    {
+        question: 'Quantos titulos brasileiro tem o corinthians?',
+        options: [
+            '2',
+            '4',
+            '7'
+        ],
+        answer: 2
+    },
+    {
+        question: 'Em que ano o corinthians foi campeao mundial contra o chelsea',
+        options: [
+            '2000',
+            '2012',
+            '2013',
+            '2001'
+        ],
+        answer: 1
+    },
+    {
+        question: 'Qual argentino fez sucesso com a camisa do corinthians',
+        options: [
+            'tevez',
+            'messi',
+            'di maria',
+            'aguero'
+        ],
+        answer: 0
+    },
+    {
+        question: 'Qual tecnico foi campeao da libertadores pelo corinthians',
+        options: [
+            'luxemburgo',
+            'zagalo',
+            'tite'
+        ],
+        answer: 2
+    },
+];
---
+++ script.js
@@ -0,0 +1,71 @@
+let currentQuestion = 0;
+let correctAnswer = 0;
+
+showQuestions();
+
+document.querySelector(".scoreArea button").addEventListener('click', resetEvent);
+
+function showQuestions(){
+    if(questions[currentQuestion]){
+       let q = questions[currentQuestion];
+       let pct = Math.floor((currentQuestion / questions.length) * 100);
+
+       document.querySelector('.progress--bar').style.width = `${pct}%`;
+
+       document.querySelector('.scoreArea').style.display = 'none';
+       document.querySelector('.questionArea').style.display = 'block';
+       document.querySelector('.question').innerHTML = q.question;
+       let optionsHtml = '';
+          for(let i in q.options){
+            optionsHtml += `<div data-op="${i}" class="option"><span>${parseInt(i)+1}</span>${q.options[i]}</div>`;
+
+          }
+
+          document.querySelector('.options').innerHTML= optionsHtml;
+
+          document.querySelectorAll('.options .option').forEach(item =>{
+             item.addEventListener('click', optionClickEvent);
+          });
+    }else{
+
+        finishQuiz();
+    }
+
+    
+}
+
+function optionClickEvent(e){
+    let clickedOption =  parseInt(e.target.getAttribute('data-op'));
+      if(questions[currentQuestion].answer === clickedOption){
+          correctAnswer++;
+      }
+      currentQuestion++;
+      showQuestions();
+}
+function finishQuiz(){
+    let points = Math.floor((correctAnswer/questions.length) * 100);
+
+    if(points > 30 && points < 60 ){
+        document.querySelector('.scoreText1').innerHTML = "Muito bom";
+        document.querySelector('.scorePct').style.color = "#ffff00";
+    }else if(points < 30){
+       
+        document.querySelector('.scoreText1').innerHTML = "Ta ruim hein?!";
+        document.querySelector('.scorePct').style.color = "red";
+    }else if(points > 60){
+        document.querySelector('.scoreText1').innerHTML = "Parabéns";
+        document.querySelector('.scorePct').style.color = "green";
+    }
+
+    document.querySelector('.scorePct').innerHTML = `Acertou ${points}%`;
+    document.querySelector('.scoreText2').innerHTML = `Voce respondeu ${questions.length} e acertou ${correctAnswer}`;
+    document.querySelector('.scoreArea').style.display = 'block';
+    document.querySelector('.questionArea').style.display = 'none';
+    document.querySelector('.progress--bar').style.width = `100%`;
+}
+
+function resetEvent(){
+    correctAnswer = 0;
+    currentQuestion = 0;
+    showQuestions();
+}
